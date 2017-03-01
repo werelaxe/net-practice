e17d3de604be49fe4963e03940fb9fca702d09a5
Werelaxe <integrex@yandex.ru>
2017-03-02 00:38:12 +0500

    fixes

--- client.py
+++ client.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 import socket
 
 HOST = '127.0.0.1'
--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 from time import sleep
 import socket
 import threading
