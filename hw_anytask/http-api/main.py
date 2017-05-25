a15a3d7fdf732a94bfb0f5bd3751d7bd27d5d9ee
melon <integrex@yandex.ru>
2017-05-25 23:41:58 +0500

    remove token from public

--- hw_anytask/http-api/main.py
+++ hw_anytask/http-api/main.py
@@ -1,9 +1,8 @@
 import vkapi
-import vk_auth
 
 
 def main():
-    api = vkapi.VKApi("friends", "b41768cf00012868fa94accf39e98b5f9b9a85b382e83871b863dc61f0283f2effd47382b0b96aec8f454")
+    api = vkapi.VKApi("friends")
     print(api.friends.get(user_id=26941815))
 
 
